bd8420c3692d0cdfc2d156c22bf60361dda60e1a
jwaher-x <jawaher.5alidfnn@gmail.com>
2026-03-25 18:44:04 +0300

    Added navbar with its style

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="logo">
+        <Link to="/" className="logo-link">Medad</Link>
+      </div>
+      <ul className="nav-links">
+        <li><Link to="/" className="nav-item">Home</Link></li>
+        <li><Link to="/login" className="nav-item">Login</Link></li>
+        <li>
+          <Link to="/register-role" className="signup-btn">Sign Up</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+export default Navbar;
