10ae787c773aec59613c2a06b1434ae49d960d6b
jwaher-x <jawaher.5alidfnn@gmail.com>
2026-04-29 19:08:31 +0300

    fix feedback route

--- server/routes/feedbackRoutes.js
+++ server/routes/feedbackRoutes.js
@@ -1,19 +1,30 @@
 const express = require('express');
 const router = express.Router();
 const Feedback = require('../models/Feedback');
+const Request = require('../models/Request');
 
-router.post('/', async (req, res) => {
+router.post('/submit', async (req, res) => {
+  const { requestId, rating, comment } = req.body;
+  
   try {
-    const newFeedback = new Feedback(req.body);
+    
+    const request = await Request.findById(requestId);
+    if (!request || request.status !== 'Delivered') {
+      return res.status(400).json({ error: 'Cannot rate before delivery' });
+    }
+
+    const newFeedback = new Feedback({
+      restaurantId: request.restaurantId,
+      charityId: request.charityId,
+      requestId,
+      rating,
+      comment
+    });
+
     await newFeedback.save();
     res.status(201).json(newFeedback);
   } catch (err) {
     res.status(400).json({ error: err.message });
   }
-});
-
-router.get('/:restaurantId', async (req, res) => {
-  const feedback = await Feedback.find({ restaurantId: req.params.restaurantId });
-  res.json(feedback);
 });
 module.exports = router;
